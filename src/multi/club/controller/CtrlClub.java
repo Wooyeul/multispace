5c4576b7a8d6f45115308be0806696a40a577eb3
sehwan <student@M13063>
2018-01-23 19:03:41 +0900

    세환 마지막 getrealpath 커밋

--- src/multi/club/controller/CtrlClub.java
+++ src/multi/club/controller/CtrlClub.java
@@ -107,7 +107,8 @@ public class CtrlClub {
 	@ResponseBody
 	public String club_add_submit(HttpServletRequest request) throws Exception {
 		try{
-			String savePath = "C:\\Users\\student\\git\\msspace_01\\WebContent\\club_img";
+			//String savePath = "C:\\Users\\student\\git\\msspace_01\\WebContent\\club_img";
+			String savePath = request.getServletContext().getRealPath("club_img");
 		    int sizeLimit = 1024*1024*15;
 		    MultipartRequest mpr = new MultipartRequest(request, savePath, sizeLimit, "utf-8", new DefaultFileRenamePolicy());
 		    ClubVO pvo = new ClubVO();	      
